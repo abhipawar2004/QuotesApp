0972ab915ae576cbd5d4cf507c9448d5c99da474
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-07-09 13:07:16 +0530

    chore: Update HomeScreen with gradient background, styled quote card, and add Flutter-related files for Linux, macOS, and Windows

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,35 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double height = 40.0;
+    double width = 40.0;
     return Scaffold(
-      appBar: AppBar(title: const Text('QUOTES'), centerTitle: true),
+      backgroundColor: Colors.black,
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xff40c9ff),
-              Color(0xffe81cff),
-            ],
-          ),
-        ),
-        child: const Column(
+        margin: EdgeInsets.only(left: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Stack(
-              children: [
-                Card(
-                  color: Colors.white,
-                  elevation: 2,
-                  child: Text(
-                      'The greatest glory in living lies not in never falling, but in rising every time we fall.'),
-                ),
-              ],
+            Spacer(),
+            Image.asset(
+              'image/s.png',
+              color: Colors.white,
+              width: width,
+              height: height,
             ),
+            SizedBox(
+              height: 50,
+            ),
+            RichText(
+              text: TextSpan(
+                style: GoogleFonts.lato(
+                  textStyle: 
+                   TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+                ),
+                
+                children: [
+                  TextSpan(text: 'Get\n'),
+                  TextSpan(
+                    text: 'Inspired',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Let"s go',
+                    style: TextStyle(color: Colors.black),
+                  )),
+            ),
+            Spacer(),
           ],
         ),
       ),
